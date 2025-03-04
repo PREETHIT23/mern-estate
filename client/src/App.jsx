9f14eebb4a58ffcd25520c742a2ffaa457790093
PREETHIT23 <preethithangavel2006@gmail.com>
2025-03-04 23:35:04 +0530

    complete sign up page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,9 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Profile from "./pages/Profile";
 import About from "./pages/About";
-import SignUp from "./pages/Signup";
-import SignIn from "./pages/Signin";
-import Header from "./components/Header";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Header from "./components/Header.jsx";
 
 export default function App() {
   return <BrowserRouter>
